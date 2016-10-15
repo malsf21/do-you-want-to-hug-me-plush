6c0561ae83a8ead9b2b64ae78dd829147b7c30f9
simonguozirui <simon.guo@10208.local>
2016-10-15 15:21:28 -0400

    everything works!

--- hardware/bluetooth_testing/bluetooth_testing.ino
+++ hardware/bluetooth_testing/bluetooth_testing.ino
@@ -1,4 +1,3 @@
-//import libraries
 #include <BLEAttribute.h>
 #include <BLECentral.h>
 #include <BLECharacteristic.h>
@@ -7,32 +6,22 @@
 #include <BLEPeripheral.h>
 #include <BLEService.h>
 #include <BLETypedCharacteristic.h>
+#include <BLETypedCharacteristics.h>
 #include <BLEUuid.h>
 #include <CurieBLE.h>
-//define ports
-const int Eye = 2;
-const int TouchPin_1=3;
-const int TouchPin_2=4;
-const int TouchPin_3=5;
-const int TouchPin_4=6;
-const int TouchPin_5=7;
-const int TouchPin_6=8;
-int sum_input;
-//set up bluetooth
+// Create my own UUIDs; used https://www.uuidgenerator.net/
 BLEService statusService("2947af14-fc38-11e5-86aa-5e5517507c66");
 #define status_UUID "2947af14-fc38-11e5-86aa-5e5517507c66"
+// Arduino 101 acts as a BLE peripheral
 BLEPeripheral blePeripheral;
 BLEIntCharacteristic statusval(status_UUID, BLERead | BLENotify);
 
-void setup() { 
+// Assign pin to indicate BLE connection 
+const int INDICATOR_PIN = 2;
+
+void setup() {
   Serial.begin(9600);
-  pinMode(Eye, OUTPUT);
-  pinMode(TouchPin_1, INPUT);
-  pinMode(TouchPin_2, INPUT);
-  pinMode(TouchPin_3, INPUT);
-  pinMode(TouchPin_4, INPUT);
-  pinMode(TouchPin_5, INPUT);
-  pinMode(TouchPin_6, INPUT);
+ 
   // Initialize BLE peripheral
   blePeripheral.setLocalName("Plush");
   blePeripheral.setAdvertisedServiceUuid(statusService.uuid());
@@ -45,31 +34,25 @@ void setup() {
   Serial.println("Bluetooth device active, waiting for connections...");
 }
 
-
-void updatestatus() {
-  statusval.setValue(sum_input);
-}
-
 void loop() {
+  // Check if the connection to the central is active or not
   BLECentral central = blePeripheral.central();
-  sum_input = digitalRead(TouchPin_1)+digitalRead(TouchPin_2)+digitalRead(TouchPin_3)+digitalRead(TouchPin_4)+digitalRead(TouchPin_5)+digitalRead(TouchPin_6);
-  Serial.println(sum_input);
   if(central) {
     Serial.print("Connected to central: ");
     Serial.println(central.address());
-    updatestatus();
-    while(sum_input != 0 && central.connected()){
-      for (int i=0; i < 3; i++){
-        digitalWrite(Eye, HIGH);    
-        delay(100); 
-        digitalWrite(Eye, LOW);  
-        delay(100); 
-        Serial.println("flag");
-      }    
-    sum_input = 0; 
+    digitalWrite(INDICATOR_PIN, HIGH);  
+    while(central.connected()) {
+      updatestatus();
+    }
+    
     Serial.print("Disconnected from central: ");
     Serial.println(central.address());
-    digitalWrite(Eye, LOW);    
-    }
+    digitalWrite(INDICATOR_PIN, LOW);
   }
 }
+
+void updatestatus() {
+  statusval.setValue(1);
+}
+
+
